c54ff1e4109a129ec5620d1fe23533f353c6ac35
YongGyunKim-Croc <yonggyun.kim@tictoccroc.com>
2021-10-24 01:23:56 +0900

    english

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-a = Image.open("week4_3.jpg")
-result = pytesseract.image_to_string(a,lang='kor')
+a = Image.open("tt.png")
+result = pytesseract.image_to_string(a)
 print(result)
