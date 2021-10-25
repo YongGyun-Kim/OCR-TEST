1acbcd55ccba2d35d659f6f465173de65972f8a7
YongGyunKim-Croc <yonggyun.kim@tictoccroc.com>
2021-10-25 11:26:33 +0900

    pdf

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import pytesseract
 from PIL import Image
 
+
+# pytesseract pillow pymupdf
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
-a = Image.open("tt.png")
-result = pytesseract.image_to_string(a)
+# import fitz
+# doc = fitz.open("aa.pdf")
+# page = doc.load_page(1)
+# mat = fitz.Matrix(2,2)
+# pix = page.get_pixmap(matrix = mat)
+# output = "aa.png"
+# pix.save(output)
+
+image = Image.open("tt.png")
+result = pytesseract.image_to_string(image)
 print(result)
